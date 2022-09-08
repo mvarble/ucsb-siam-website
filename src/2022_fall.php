8267896a5a8abf92c5f3103aa86c3b44e88b5d33
ZachWagner <30417821+ZachWagner@users.noreply.github.com>
2022-09-08 13:16:43 -0700

    New page for Fall 2022

---
+++ src/2022_fall.php
@@ -0,0 +1,103 @@
+<?php include_once('scripts/functions.php'); ?>
+<?php write_header("Fall 2022 Seminar"); ?>
+
+<div id="cell">
+
+	<h1>Fall 2022 - Seminar in Applied Mathematics</h1>
+				
+	<p>The UCSB student chapter of the Society for Industrial and Applied Mathematicians (SIAM) will host a weekly, interdisciplinary seminar to provide a forum for graduate students to discuss their research in an informal setting. 
+This is a great outlet to polish your presentation skills in front of a general (technical) audience. 
+</p>
+	<p>If you are interested in giving a talk during this seminar, please send an email to <a href="mailto:siam@math.ucsb.edu?Subject=Fall Seminar" target="_top">siam@math.ucsb.edu</a> with a potential title, abstract, and when you would like to speak.</p>
+	<br>
+	<p>All are welcome to attend, cookies and coffee provided.</a></p>
+	<br>
+	<p><b>When</b>: Mondays 2-3</p>
+	<p><b>Where</b>: South Hall 6635</p>
+
+
+	<table id="hor-minimalist-b">
+	<tr>
+	<td><h2>Date</h2></td>
+	<td><h2>Fall 2022 Schedule</h2></td>
+	</tr>
+
+    <?php  organize("September&nbsp;26th"); ?>
+
+    <?php speaker_data("October&nbsp;3rd",
+    "",
+    "",
+    "",
+    "" ); ?>
+    
+    <?php speaker_data("October&nbsp;10th",
+    "",
+    "",
+    "",
+    "" ); ?>        
+    
+    <?php speaker_data("October&nbsp;17th",
+    "",
+    "",
+    "",
+    "" ); ?>
+
+    <?php speaker_data("October&nbsp;24th",
+    "",
+    "",
+    "",
+    "" ); ?>
+
+    <?php speaker_data("October&nbsp;31th",
+    "",
+    "",
+    "",
+    "" ); ?>
+
+    <?php speaker_data("November&nbsp;7th",
+    "",
+    "",
+    "",
+    "" ); ?>
+
+    <?php speaker_data("November&nbsp;14th",
+    "",
+    "",
+    "",
+    "" ); ?>
+
+    <?php speaker_data("November&nbsp;21st",
+    "",
+    "",
+    "",
+    "" ); ?>
+    
+    <?php speaker_data("November&nbsp;28th",
+    "",
+    "",
+    "",
+    "" ); ?>
+    
+    <?php speaker_data("December&nbsp;5th",
+    "",
+    "",
+    "",
+    "" ); ?>
+    
+    <?php speaker_data("December&nbsp;12th",
+    "",
+    "",
+    "",
+    "" ); ?>
+    
+    <?php #no_seminar("May&nbsp;28th"); ?>
+
+    <?php  #organize("June&nbsp;4th"); ?>
+
+				
+	</table>
+
+</div>
+
+	
+<?php write_footer(); ?>
